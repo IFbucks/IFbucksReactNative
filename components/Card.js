0d33e46df7432cf7ba9c955f82604f51bb9d6252
mariagerotti <mariaeduarda.gerotti@gmail.com>
2023-03-27 18:34:22 -0300

    criação do projeto

--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,56 @@
-import { View } from "react-native-web";
-import { Component } from "react";
-import { Text } from "react-native-web";
+import React, { useState } from 'react';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
+const PhotoCard = ({ imageProduct }) => {
+  const [add, setAdd] = useState(0);
 
-function Card (props) {
-    return (
-        <View style={StyleSheet.card}>
-           <Image style={{resizeMode: 'stretch', height: '200'}} source={{uri:props.capa}}/> 
-           <Text>{props.nomeProduto}</Text>
-           <Text>R${props.precoProduto}</Text>
-        </View>
+  const handleaddPress = () => {
+    setAdd(add + 1);
+  };
 
-        
-    )
-}
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: imageProduct }} style={styles.image} />
+      <View style={styles.addContainer}>
+        <TouchableOpacity onPress={handleaddPress}>
+          <Text style={styles.add}> <button> + {add} adicionar</button></Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
-export default Card
+const styles = StyleSheet.create({
+  card: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginBottom: 20,
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+  },
+  likesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  add: {
+    fontSize: 20,
+    color: '#333',
+    marginLeft: 10,
+  },
+  button: {
+   borderColor: 'black'
+  }
+});
+
+export default PhotoCard;
--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   headerText: {
-    color: 'white',
-    fontSize: 20,
+    color: 'black',
+    fontSize: 25,
   },
 });
