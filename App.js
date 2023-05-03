e1f82157a2b3d0cce09f26f45308b3b103cf7225
isagrt <isadoragoedert@gmail.com>
2023-05-03 13:36:48 -0300

    alt background

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#e1ede4',
     padding: 20,
   },
   title: {
